aa97134f7bea7c20d28550cacbe2e84480565d73
Sasha Haywood <haywood@iu.edu>
2021-02-18 15:37:43 -0500

    fixing class

--- citations.R
+++ citations.R
@@ -1,1 +1,9 @@
+
 citations = read.csv("citation_data.csv")
+
+citations$First.Name = as.character(citations$First.Name)
+citations$Last.Name = as.character(citations$Last.Name)
+citations$Citation.Count = as.integer(citations$Citation.Count)
+
+male.auth = subset(citations, Gender.of.Article.Author == 1)
+female.auth = subset(citations, Gender.of.Article.Author == 0)
