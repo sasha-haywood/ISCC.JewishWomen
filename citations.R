0ff95be838c1159f024b43be97b8b30a7cd1141a
Sasha Haywood <haywood@iu.edu>
2021-02-19 09:38:38 -0500

    I think the citations data.frame is finalized now.  I suggest we both use the same data frame.  Empty rows have been removed, and appropriate variables have been converted to factors and have been renamed.

--- citations.R
+++ citations.R
@@ -1,24 +1,45 @@
 library(ggplot2)
-citations = read.csv("citation_data.csv")
+library(dplyr)
+citations = read.csv("citation_data.csv", colClasses = c("character", "character", 
+                                                         "integer", rep("factor",7)))
 
-citations$First.Name = as.character(citations$First.Name)
-citations$Last.Name = as.character(citations$Last.Name)
-citations$Citation.Count = as.integer(citations$Citation.Count)
+levels(citations$Gender.of.Article.Author) = list("Female" = 0, "Male" = 1)
+levels(citations$Gender.of.Citation) = list("Female" = 0, "Male" = 1)
+levels(citations$field) = list("History" = 1, "Philosophy/MJT" = 2, 
+                               "Literature" = 3, "Ancient Text" = 4, 
+                               "Sociology/Ethnography/Contemporary" = 5)
+levels(citations$time.period) = list("Ancient" = 1, "Medieval" = 2, "Modern" =3)
 
-male.auth = subset(citations, Gender.of.Article.Author == 1)
-female.auth = subset(citations, Gender.of.Article.Author == 0)
+male.auth = subset(citations, Gender.of.Article.Author == "Male")
+female.auth = subset(citations, Gender.of.Article.Author == "Female")
 
 # Is the average number of female citations = to the known presence in the field?
-t.test(citations$Gender.of.Citation==0, alternative = "less", mu=.42)
+t.test(citations$Gender.of.Citation=="Female", alternative = "less", mu=.42)
 # Among male authors?
-t.test(male.auth$Gender.of.Citation==0, alternative = "less", mu=.42)
+t.test(male.auth$Gender.of.Citation=="Female", alternative = "less", mu=.42)
 # Among female authors?
-t.test(female.auth$Gender.of.Citation==0, alternative = "less", mu=.42)
+t.test(female.auth$Gender.of.Citation=="Female", alternative = "less", mu=.42)
 # Is there a significant difference in % female citations between male and female authors?
-t.test(male.auth$Gender.of.Citation==0, female.auth$Gender.of.Citation==0)
+t.test(male.auth$Gender.of.Citation=="Female", female.auth$Gender.of.Citation=="Female")
 # All p-values are extremely low.  There is almost 0 chance that this is by chance.
 # Female authors are cited less than 42% of the time regardless of gender of the author
 # and are cited significantly less often by male authors.
 
+# Basic plot to start with
 ggplot(citations, aes(x=Gender.of.Citation)) +
-  geom_bar()
+  geom_bar(aes(y = (..count..)/sum(..count..))) +
+  scale_y_continuous(labels = scales::percent) +
+  ylab("Percent") +
+  xlab("Gender of Cited Author") +
+  ggtitle("Gender of Cited Authors")
+
+ggplot(citations, aes(x=Gender.of.Citation)) +
+  geom_bar(aes(y = (..count..)/sum(..count..), fill=Gender.of.Article.Author)) +
+  scale_y_continuous(labels = scales::percent) +
+  ylab("Percent") +
+  xlab("Gender of Cited Author") +
+  ggtitle("Gender of Cited Authors") +
+  labs(fill = "Gender of Article Author")
+
+
+
