7c656e5504bdeba1d775fd4fe1f490e5e17e7b7b
Sasha Haywood <haywood@iu.edu>
2021-02-24 15:30:18 -0500

    BW and color for each plot

--- citations.R
+++ citations.R
@@ -3,6 +3,7 @@ library(dplyr)
 library(ggmosaic)
 library(tidyr)
 
+
 citations = read.csv("citation_data.csv", colClasses = c("character", "character", 
                                                          "integer", rep("factor",7)))
 
@@ -12,6 +13,168 @@ levels(citations$field) = list("History" = 1, "Philosophy/ \n MJT" = 2,
                                "Literature" = 3, "Ancient Text" = 4, 
                                "Sociology/ \n Ethnography/ \n Contemporary" = 5)
 levels(citations$time.period) = list("Ancient" = 1, "Medieval" = 2, "Modern" =3)
+levels(citations$JS.or.RS) = list("Jewish Studies" = "JS", "Religious Studies" = "RS")
+
+#1 b/w
+ggplot(citations, aes(x=Gender.of.Citation)) +
+  geom_bar(aes(y = (..count..)/sum(..count..), fill=Gender.of.Article.Author)) +
+  scale_y_continuous(labels = scales::percent) +
+  ylab("Percent") +
+  xlab("Gender of Cited Author") +
+  ggtitle("Gender of Cited Authors") +
+  geom_hline(yintercept = .42, linetype = "dotted") +
+  labs(fill = "Gender of Article Author",
+       subtitle = "dotted line at 42%") +
+  scale_fill_grey(start = .5, end = 0)
+
+#1 color
+ggplot(citations, aes(x=Gender.of.Citation)) +
+  geom_bar(aes(y = (..count..)/sum(..count..), fill=Gender.of.Article.Author)) +
+  scale_y_continuous(labels = scales::percent) +
+  ylab("Percent") +
+  xlab("Gender of Cited Author") +
+  ggtitle("Gender of Cited Authors") +
+  geom_hline(yintercept = .42, linetype = "dotted") +
+  labs(fill = "Gender of Article Author",
+       subtitle = "dotted line at 42%") +
+  scale_fill_manual(values=c("indianred1","blue")) 
+
+
+#2 b/w
+ggplot(citations, aes(x=Gender.of.Article.Author, fill = forcats::fct_rev(Gender.of.Citation))) +
+  geom_bar(position = "fill") +
+  scale_y_continuous(labels = scales::percent) +
+  ylab("Percent") +
+  xlab("Article Author") +
+  ggtitle("Gender of Cited Authors by Gender of Article Author") +
+  labs(fill = "Cited Author",
+       subtitle = "dotted line at 42%") +
+  geom_hline(yintercept = .42, linetype = "dotted") +
+  scale_fill_grey(start = .5, end = 0)
+
+#2 color
+ggplot(citations, aes(x=Gender.of.Article.Author, fill = forcats::fct_rev(Gender.of.Citation))) +
+  geom_bar(position = "fill") +
+  scale_y_continuous(labels = scales::percent) +
+  ylab("Percent") +
+  xlab("Article Author") +
+  ggtitle("Gender of Cited Authors by Gender of Article Author") +
+  labs(fill = "Cited Author",
+       subtitle = "dotted line at 42%") +
+  geom_hline(yintercept = .42, linetype = "dotted") +
+  scale_fill_manual(values=c("royalblue2", "indianred1"))
+
+#3 b/w
+ggplot(citations) +
+  geom_mosaic(aes(x=product(field), fill = Gender.of.Citation)) +
+  scale_y_continuous(labels = scales::percent) +
+  ylab("Percent") +
+  xlab("Field") +
+  ggtitle("Gender of Cited Authors by Field") +
+  labs(fill = "Gender of Cited Author",
+       subtitle = "dotted line at 42%") +
+  geom_hline(yintercept = .42, linetype = "dotted") +
+  scale_fill_grey(start = 0, end = .5) +
+  coord_flip()
+
+#3 color
+ggplot(citations) +
+  geom_mosaic(aes(x=product(field), fill = Gender.of.Citation)) +
+  scale_y_continuous(labels = scales::percent) +
+  ylab("Percent") +
+  xlab("Field") +
+  ggtitle("Gender of Cited Authors by Field") +
+  labs(fill = "Gender of Cited Author",
+       subtitle = "dotted line at 42%") +
+  geom_hline(yintercept = .42, linetype = "dotted") +
+  scale_fill_manual(values=c("indianred1", "royalblue2")) +
+  coord_flip()
+
+#4 b/w
+ggplot(na.omit(citations)) +
+  geom_mosaic(aes(x=product(time.period), fill = Gender.of.Citation)) +
+  scale_y_continuous(labels = scales::percent) +
+  ylab("Percent") +
+  xlab("Time Period") +
+  ggtitle("Gender of Cited Authors by Time Period") +
+  labs(fill = "Gender of Cited Author",
+       subtitle = "dotted line at 42%") +
+  geom_hline(yintercept = .42, linetype = "dotted") +
+  scale_fill_grey(start = 0, end = .5) +
+  coord_flip()
+  
+#4 color
+ggplot(na.omit(citations)) +
+  geom_mosaic(aes(x=product(time.period), fill = Gender.of.Citation)) +
+  scale_y_continuous(labels = scales::percent) +
+  ylab("Percent") +
+  xlab("Time Period") +
+  ggtitle("Gender of Cited Authors by Time Period") +
+  labs(fill = "Gender of Cited Author",
+       subtitle = "dotted line at 42%") +
+  geom_hline(yintercept = .42, linetype = "dotted") +
+  scale_fill_manual(values=c("indianred1", "royalblue2")) +
+  coord_flip()
+
+#5a b/w
+ggplot(citations, aes(x=JS.or.RS, fill = forcats::fct_rev(Gender.of.Citation))) +
+  geom_bar(position = "fill") +
+  scale_y_continuous(labels = scales::percent) +
+  ylab("Percent") +
+  xlab("Journal") +
+  ggtitle("Gender of Cited Authors by Journal") +
+  labs(fill = "Cited Author",
+       subtitle = "dotted line at 42%") +
+  geom_hline(yintercept = .42, linetype = "dotted") +
+  scale_fill_grey(start = .5, end = 0)
+
+#5a color
+ggplot(citations, aes(x=JS.or.RS, fill = forcats::fct_rev(Gender.of.Citation))) +
+  geom_bar(position = "fill") +
+  scale_y_continuous(labels = scales::percent) +
+  ylab("Percent") +
+  xlab("Journal") +
+  ggtitle("Gender of Cited Authors by Journal") +
+  labs(fill = "Cited Author",
+       subtitle = "dotted line at 42%") +
+  geom_hline(yintercept = .42, linetype = "dotted") +
+  scale_fill_manual(values=c("royalblue2", "indianred1"))
+
+#5b b/w
+ggplot(citations) +
+  geom_mosaic(aes(x=product(Gender.of.Citation, JS.or.RS, Gender.of.Article.Author), 
+                  fill = Gender.of.Citation),
+              divider = ddecker()) +
+  scale_y_continuous(labels = scales::percent) +
+  ylab("Percent") +
+  xlab("Journal and Gender of Author") +
+  ggtitle("Gender of Cited Authors by Journal and Gender of Author") +
+  labs(fill = "Gender of Cited Author",
+       subtitle = "dotted line at 42%") +
+  geom_hline(yintercept = .42, linetype = "dotted") +
+  scale_fill_grey(start = 0, end = .5) +
+  coord_flip()
+
+#5b color
+ggplot(citations) +
+  geom_mosaic(aes(x=product(Gender.of.Citation, JS.or.RS, Gender.of.Article.Author), 
+                  fill = Gender.of.Citation),
+                  divider = ddecker()) +
+  scale_y_continuous(labels = scales::percent) +
+  ylab("Percent") +
+  xlab("Journal and Gender of Author") +
+  ggtitle("Gender of Cited Authors by Journal and Gender of Author") +
+  labs(fill = "Gender of Cited Author",
+       subtitle = "dotted line at 42%") +
+  geom_hline(yintercept = .42, linetype = "dotted") +
+  scale_fill_manual(values=c("indianred1", "royalblue2")) +
+  coord_flip()
+
+
+
+
+
+
 
 # Basic plot to start with
 ggplot(citations, aes(x=Gender.of.Citation)) +
@@ -21,7 +184,8 @@ ggplot(citations, aes(x=Gender.of.Citation)) +
   xlab("Gender of Cited Author") +
   ggtitle("Gender of Cited Authors") +
   labs(fill = "Gender of Article Author") +
-  scale_fill_manual(values=c("indianred1","royalblue2"))
+  geom_hline(yintercept = .42, linetype = "dotted") +
+  scale_fill_grey(start = .5, end = 0)
 
 # I wouldn't use this one.  I think the next is better.
 ggplot(citations, aes(x=Gender.of.Article.Author)) +
